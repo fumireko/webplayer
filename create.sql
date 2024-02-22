b79d14ac683d9fba4320f3f606b3766b17bab5b4
fumireko <fumireko@yandex.com>
2024-02-22 11:22:03 -0300

    change sql

--- create.sql
+++ create.sql
@@ -1,59 +1,100 @@
---
--- PostgreSQL database dump
---
+CREATE TABLE IF NOT EXISTS tb_roles (
+    id_role SERIAL PRIMARY KEY,
+    role_name VARCHAR(255) NOT NULL UNIQUE
+);
 
--- Dumped from database version 16.1
--- Dumped by pg_dump version 16.1
+CREATE TABLE IF NOT EXISTS tb_users (
+    id_user SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
+CREATE TABLE IF NOT EXISTS tb_user_roles (
+    fk_user INT,
+    fk_role INT,
+    FOREIGN KEY (fk_user) REFERENCES tb_users(id_user) ON DELETE CASCADE,
+    FOREIGN KEY (fk_role) REFERENCES tb_roles(id_role) ON DELETE CASCADE,
+    PRIMARY KEY (fk_user, fk_role)
+);
 
---
--- Data for Name: roles; Type: TABLE DATA; Schema: public; Owner: postgres
---
+CREATE TABLE IF NOT EXISTS tb_songs (
+    id_song SERIAL PRIMARY KEY,
+    song_title VARCHAR(512),
+    song_length INTEGER,
+    fk_album INTEGER,
+    FOREIGN KEY (fk_album) REFERENCES tb_albums(id_album) ON DELETE CASCADE
+);	
 
-INSERT INTO public.roles (name, id) VALUES ('ROLE_USER', 1);
-INSERT INTO public.roles (name, id) VALUES ('ROLE_MODERATOR', 2);
-INSERT INTO public.roles (name, id) VALUES ('ROLE_ADMIN', 3);
+CREATE TABLE IF NOT EXISTS tb_albums (
+    id_album SERIAL PRIMARY KEY,
+    album_title VARCHAR(512) NOT NULL,
+	album_image_url VARCHAR(512),
+    release_date TIMESTAMP,
+    fk_artist INTEGER,
+    FOREIGN KEY (fk_artist) REFERENCES tb_artists(id_artist) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS tb_artists (
+    id_artist SERIAL PRIMARY KEY,
+    artist_name VARCHAR(512) NOT NULL UNIQUE,
+    description TEXT, 
+    country_code CHAR(2)
+);
+
+CREATE TABLE IF NOT EXISTS tb_playlists (
+    id_playlist SERIAL PRIMARY KEY,
+    playlist_name VARCHAR(100),
+    fk_user INT,
+    FOREIGN KEY (fk_user) REFERENCES tb_users(id_user) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS tb_genres (
+    id_genre SERIAL PRIMARY KEY,
+    description VARCHAR(128) UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS tb_songs_album (
+    fk_song INT,
+    fk_album INT,
+    PRIMARY KEY (fk_song, fk_album),
+    FOREIGN KEY (fk_song) REFERENCES tb_songs(id_song) ON DELETE CASCADE,
+    FOREIGN KEY (fk_album) REFERENCES tb_albums(id_album) ON DELETE CASCADE
+);
+
+	CREATE TABLE IF NOT EXISTS tb_albums_artist (
+		fk_album INT,
+		fk_artist INT,
+		PRIMARY KEY (fk_album, fk_artist),
+		FOREIGN KEY (fk_album) REFERENCES tb_albums(id_album) ON DELETE CASCADE,
+		FOREIGN KEY (fk_artist) REFERENCES tb_artists(id_artist) ON DELETE CASCADE
+	);
+
+CREATE TABLE IF NOT EXISTS tb_songs_playlist (
+    fk_song INT,
+    fk_playlist INT,
+    date_added TIMESTAMP,
+    play_count INT,
+    PRIMARY KEY (fk_song, fk_playlist),
+    FOREIGN KEY (fk_song) REFERENCES tb_songs(id_song) ON DELETE CASCADE,
+    FOREIGN KEY (fk_playlist) REFERENCES tb_playlists(id_playlist) ON DELETE CASCADE
+);
 
 
---
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
---
+CREATE TABLE IF NOT EXISTS tb_artists_genre (
+    fk_artist INT,
+    fk_genre INT,
+    PRIMARY KEY (fk_artist, fk_genre),
+    FOREIGN KEY (fk_artist) REFERENCES tb_artists(id_artist) ON DELETE CASCADE,
+    FOREIGN KEY (fk_genre) REFERENCES tb_genres(id_genre) ON DELETE CASCADE
+);
 
-INSERT INTO public.users (id, username, email, password) VALUES (1, 'test', 'test@mail.com', '$2a$10$dWcfKaUfufIpMqom1Vxije2fWi.1IluXFQAxbFBS9af2xP1yX.g22');
+INSERT INTO public.tb_roles (id_role, role_name) VALUES (1, 'ROLE_USER');
+INSERT INTO public.tb_roles (id_role, role_name) VALUES (2, 'ROLE_MODERATOR');
+INSERT INTO public.tb_roles (id_role, role_name) VALUES (3, 'ROLE_ADMIN');
 
+INSERT INTO public.tb_users (id_user, username, email, password) VALUES (1, 'test', 'test@mail.com', '$2a$10$dWcfKaUfufIpMqom1Vxije2fWi.1IluXFQAxbFBS9af2xP1yX.g22');
 
---
--- Data for Name: user_roles; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-INSERT INTO public.user_roles (user_id, role_id) VALUES (1, 1);
-
-
---
--- Name: roles_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.roles_id_seq', 3, true);
-
-
---
--- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.users_id_seq', 1, true);
-
-
---
--- PostgreSQL database dump complete
---
-
+INSERT INTO public.tb_user_roles (fk_user, fk_role) VALUES (1, 1);
+INSERT INTO public.tb_user_roles (fk_user, fk_role) VALUES (1, 2);
+INSERT INTO public.tb_user_roles (fk_user, fk_role) VALUES (1, 3);
