dbad48dafa0c7df3407dc5ff3e6d15cc84f5ff5c
fumireko <fumireko@yandex.com>
2024-02-23 10:49:39 -0300

    change column length on artist model

--- server/src/main/java/ca/fubi/player/models/Artist.java
+++ server/src/main/java/ca/fubi/player/models/Artist.java
@@ -25,7 +25,7 @@ public class Artist {
     @Column(name = "description")
     private String description;
 
-    @Column(name = "country_code", columnDefinition = "CHAR(2)")
+    @Column(name = "country_code", columnDefinition = "CHAR(4)")
     private EnumCountry countryCode;
 
     @OneToMany(mappedBy = "artist")
