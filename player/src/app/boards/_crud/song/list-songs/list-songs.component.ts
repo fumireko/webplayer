65d8b7472dcf765c2f5d9d35b21a5862aeb042e5
fumireko <fumireko@yandex.com>
2024-03-14 10:36:21 -0300

    fix song listing component

--- player/src/app/boards/_crud/song/list-songs/list-songs.component.ts
+++ player/src/app/boards/_crud/song/list-songs/list-songs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ApiService } from '../../../../services/api.service';
 import { Song } from '../../../../shared/models/song.model';
 import { Album } from '../../../../shared/models/album.model';
@@ -10,63 +10,91 @@ import { Album } from '../../../../shared/models/album.model';
 })
 export class ListSongsComponent {
 
+  @Input() songData: Album | null = null;
+  @Output() songDataChange: EventEmitter<Album | null> = new EventEmitter<Album | null>();
+
   editSong: boolean = false;
   editAlbum: boolean = false;
   selectedSong: Song = new Song();
   selectedSongAlbum: Album = new Album();
-  selectedSongReset: any;
+  selectedSongReset: string = "";
+
   songs: Song[] = [];
   albums: Album[] = [];
+  resetSongs: string = "";
+  resetAlbums: string = "";
 
   constructor(private http: ApiService){}
 
   ngOnInit(){
     this.http.getAlbums().subscribe((data: Album[]) => {
+      this.resetAlbums = JSON.stringify(data);
       this.albums = data;
     })
     this.http.getSongs().subscribe((data: Song[]) => {
+      this.resetSongs = JSON.stringify(data);
       this.songs = data;
-      this.songs.forEach(s => {
-        if (s.album?.releaseDate) s.album.releaseDateString = s.album.releaseDate.toString().split('T')[0];
-      });
-    });
-     
+    });  
   }
 
   getSongsByAlbum(album: Album): Song[] {
     return this.songs.filter(song => song.album?.id === album.id);
   }
 
-  showDetails(_t11: Song){
-    this.selectedSong = _t11;
-    if( _t11.album) this.selectedSongAlbum = _t11.album;
-    this.selectedSongReset = _t11;
-    console.log(JSON.stringify(this.selectedSong));
+  getArtistsNames(album: Album | undefined): string {
+    if(album) if(album.artists) return album.artists?.map(artist => artist.name).join(', ');
+    return "";
   }
 
-  stringify(o: any) { return JSON.stringify(o) }
-
-  compare(a: any, b: any){
-    let expression = (a.id === b.id);
-    console.log(a.id, '+', b.id, '=', expression);
-    return expression;
+  showDetails(_t11: Song){
+    this.selectedSong = _t11;
+    this.selectedSongReset = JSON.stringify(_t11);
+    if( _t11.album) this.selectedSongAlbum = _t11.album;
   }
 
   saveSong(){
-    if(this.editSong){ console.log("Saving song: " + JSON.stringify(this.selectedSong))}
+    if(this.editSong){
+      this.selectedSongAlbum = this.selectedSong.album!;
+      this.editAlbum = !this.editAlbum;
+      this.updateListing();
+    }
     else this.editSong = !this.editSong;
   }
 
-  cancelSong() { 
-    console.log(JSON.stringify('RESET: ' + this.selectedSongReset));
-    this.selectedSong = this.selectedSongReset;
+  cancelSong() {
+    this.resetListing();
     this.editSong = false;
   }
 
-  saveAlbum(){
-    if(this.editAlbum){ console.log("Saving album: " + JSON.stringify(this.selectedSong.album))}
+  saveAlbum() {
+    if (this.editAlbum) { 
+      this.songs.filter(s => s.album?.id === this.selectedSong.album?.id).forEach(e => e.album = this.selectedSong.album);
+      this.editAlbum = !this.editAlbum;
+      this.updateListing();
+    } 
     else this.editAlbum = !this.editAlbum;
   }
 
-  cancelAlbum() { this.editAlbum = false }
+  cancelAlbum() {
+    this.resetListing();
+    this.editAlbum = false
+  }
+
+  updateListing(){
+    this.http.getAlbums().subscribe((data: Album[]) => {
+      this.resetAlbums = JSON.stringify(data);
+      this.albums = data;
+    })
+    this.http.getSongs().subscribe((data: Song[]) => {
+      this.resetSongs = JSON.stringify(data);
+      this.songs = data;
+    });
+  }
+
+  resetListing(){
+    this.songs = JSON.parse(this.resetSongs);
+    this.albums = JSON.parse(this.resetSongs);
+    this.selectedSong = JSON.parse(this.selectedSongReset);
+    this.selectedSongAlbum = this.selectedSong.album!;
+  }
 }
