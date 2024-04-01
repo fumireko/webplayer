ead60bbc7ebd1f217b4403bdbfdf339f43fbb2f5
fumireko <fumireko@yandex.com>
2024-04-01 09:56:26 -0300

    implement functionality to list genres and list playlists components

--- player/src/app/boards/_crud/genre/list-genres/list-genres.component.ts
+++ player/src/app/boards/_crud/genre/list-genres/list-genres.component.ts
@@ -1,3 +1,4 @@
+// list-genres.component.ts
 import { Component } from '@angular/core';
 import { Genre } from '../../../../shared/models/genre.model';
 import { ApiService } from '../../../../services/api.service';
@@ -5,22 +6,54 @@ import { ApiService } from '../../../../services/api.service';
 @Component({
   selector: 'app-list-genres',
   templateUrl: './list-genres.component.html',
-  styleUrl: './list-genres.component.css'
+  styleUrls: ['./list-genres.component.css']
 })
 export class ListGenresComponent {
 
-  selectedArtist: any;
+  selectedGenre: Genre | undefined;
+  newGenre: Genre = new Genre();
+  newGenreToggle: boolean = false;
+  editGenre: boolean = false;
   genres: Genre[] = [];
 
-  constructor(private http: ApiService){}
+  constructor(private http: ApiService) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.http.getGenres().subscribe((data: Genre[]) => {
       this.genres = data;
     });
   }
 
-  showDetails(_t11: Genre) {
-    this.selectedArtist = _t11;
+  showDetails(genre: Genre) {
+    this.selectedGenre = genre;
+  }
+
+  toggleNewGenre() {
+    this.newGenreToggle = !this.newGenreToggle;
+  }
+
+  toggleEditGenre() {
+    this.editGenre = !this.editGenre;
+  }
+
+  saveNewGenre() {
+    this.http.saveGenre(this.newGenre).subscribe((genre: Genre) => {
+      this.genres.push(genre);
+      this.resetEdit();
+    });
+  }
+
+  saveGenre() {
+    if (!this.selectedGenre) return;
+    this.http.updateGenre(this.selectedGenre).subscribe((genre: Genre) => {
+      if (this.selectedGenre) this.selectedGenre = genre;
+      this.resetEdit();
+    });
+  }
+
+  resetEdit() {
+    this.editGenre = false;
+    this.newGenreToggle = false;
+    this.newGenre = new Genre();
   }
 }
--- player/src/app/boards/_crud/playlist/list-playlists/list-playlists.component.ts
+++ player/src/app/boards/_crud/playlist/list-playlists/list-playlists.component.ts
@@ -1,26 +1,86 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../../../services/api.service';
 import { Playlist } from '../../../../shared/models/playlist.model';
+import { Song } from '../../../../shared/models/song.model';
+import { User } from '../../../../shared/models/user.model'; // Import User model if not already imported
 
 @Component({
   selector: 'app-list-playlists',
   templateUrl: './list-playlists.component.html',
-  styleUrl: './list-playlists.component.css'
+  styleUrls: ['./list-playlists.component.css']
 })
 export class ListPlaylistsComponent {
 
-  selectedPlaylist: any;
+  selectedPlaylist: Playlist | undefined;
+  newPlaylist: Playlist = new Playlist();
+  newSong: Song = new Song();
+  addSongToggle: boolean = false;
   playlists: Playlist[] = [];
+  songs: Song[] = [];
+  users: User[] = [];
+  newPlaylistToggle: boolean = false;
 
-  constructor(private http: ApiService){}
+  constructor(private http: ApiService) { }
 
-  ngOnInit(){
+  ngOnInit() {
     this.http.getPlaylists().subscribe((data: Playlist[]) => {
       this.playlists = data;
     });
+    this.http.getSongs().subscribe((data: Song[]) => {
+      this.songs = data;
+    });
+    this.http.getUsers().subscribe((data: User[]) => {
+      this.users = data;
+    });
   }
 
-  showDetails(_t11: Playlist) {
-    this.selectedPlaylist = _t11;
+  showDetails(playlist: Playlist) {
+    this.selectedPlaylist = playlist;
+  }
+
+  toggleAddSong() {
+    this.addSongToggle = !this.addSongToggle;
+  }
+
+  addSong() {
+    if (this.selectedPlaylist) {
+      this.selectedPlaylist.songs?.push(this.newSong);
+      this.savePlaylist();
+    }
+  }
+
+  // Define toggleNewPlaylist method
+  toggleNewPlaylist() {
+    this.newPlaylistToggle = !this.newPlaylistToggle;
+  }
+
+  saveNewPlaylist() {
+    this.http.savePlaylist(this.newPlaylist).subscribe((playlist: Playlist) => {
+      this.playlists.push(playlist);
+      this.resetEdit();
+    });
+  }
+
+  savePlaylist() {
+    if (this.selectedPlaylist) {
+      this.http.updatePlaylist(this.selectedPlaylist).subscribe((playlist: Playlist) => {
+        this.selectedPlaylist = playlist;
+        this.resetEdit();
+      });
+    }
+  }
+
+  filterSongs(): Song[] {
+    if (!this.selectedPlaylist || !this.selectedPlaylist.songs) {
+      return this.songs;
+    }
+    return this.songs.filter(song => !this.selectedPlaylist!.songs!.some(s => s.id === song.id));
+  }
+
+  resetEdit() {
+    this.addSongToggle = false;
+    this.newPlaylistToggle = false;
+    this.newPlaylist = new Playlist();
+    this.newSong = new Song();
   }
 }
