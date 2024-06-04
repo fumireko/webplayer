842754751e39b92d8ddbcef7aee3062150771bd2
fumireko <fumireko@yandex.com>
2024-06-04 16:02:40 -0300

    update http interceptor

--- player/src/app/services/auth.service.ts
+++ player/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { AppComponent } from '../app.component';
 
 const httpOptions = {
@@ -10,21 +11,26 @@ const httpOptions = {
 @Injectable({
   providedIn: 'root'
 })
-
 export class AuthService {
+  private token: string | null = null;
 
   constructor(private http: HttpClient) { }
 
   login(email: string, password: string): Observable<any> {
-    return this.http.post(AppComponent.PUBLIC_BACKEND_URL + '/users/signin',
+    return this.http.post<any>(`${AppComponent.PUBLIC_BACKEND_URL}/users/signin`,
     {
-      email,  password
+      email, password
     },
-    httpOptions);
+    httpOptions).pipe(
+      tap(response => {
+        this.token = response.token; // Salva o token após o login
+        window.sessionStorage.setItem('auth-token', this.token!); // Armazena o token no sessionStorage
+      })
+    );
   }
 
   registerUser(username: string, email: string, password: string): Observable<any> {
-    return this.http.post(AppComponent.PUBLIC_BACKEND_URL + '/users/signup',
+    return this.http.post(`${AppComponent.PUBLIC_BACKEND_URL}/users/signup`,
     {
       username,
       email,
@@ -32,11 +38,11 @@ export class AuthService {
       "roles": [{"id": "1", "name": "ROLE_USER"}]
     },
     httpOptions
-    )
+    );
   }
 
   registerModerator(username: string, email: string, password: string): Observable<any> {
-    return this.http.post(AppComponent.PUBLIC_BACKEND_URL + '/users/signup',
+    return this.http.post(`${AppComponent.PUBLIC_BACKEND_URL}/users/signup`,
     {
       username,
       email,
@@ -44,11 +50,11 @@ export class AuthService {
       "role": "ROLE_MODERATOR"
     },
     httpOptions
-    )
+    );
   }
 
   registerAdmin(username: string, email: string, password: string): Observable<any> {
-    return this.http.post(AppComponent.PUBLIC_BACKEND_URL + '/users/signup',
+    return this.http.post(`${AppComponent.PUBLIC_BACKEND_URL}/users/signup`,
     {
       username,
       email,
@@ -56,11 +62,18 @@ export class AuthService {
       "role": "ROLE_ADMIN"
     },
     httpOptions
-    )
+    );
   }
 
   logout(): Observable<any> {
-    window.sessionStorage.removeItem('auth-user')
-    return this.http.post(AppComponent.PUBLIC_BACKEND_URL + '/users/signout', { }, httpOptions);
+    window.sessionStorage.clear();
+    return this.http.post(`${AppComponent.PUBLIC_BACKEND_URL}/users/signout`, {}, httpOptions);
+  }
+
+  getToken(): string | null {
+    if (!this.token) {
+      this.token = window.sessionStorage.getItem('auth-token');
+    }
+    return this.token;
   }
 }
--- player/src/app/services/http.service.ts
+++ player/src/app/services/http.service.ts
@@ -1,15 +1,30 @@
 import { HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service'; // Importe o AuthService
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpRequestInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) { }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    req = req.clone({
-      withCredentials: true,
-    });
+    const token = this.authService.getToken(); // Obtenha o token do AuthService
+
+    if (token) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        },
+        withCredentials: true,
+      });
+    } else {
+      req = req.clone({
+        withCredentials: true,
+      });
+    }
 
     return next.handle(req);
   }
